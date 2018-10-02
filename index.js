c9e9daa153b7cb4cbb8011a711a2287ec555d8e4
Jayon Kim <jpkim921@gmail.com>
2018-10-02 19:59:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ function BoardMember(name, homestate, training){
   this.name = name
   this.homestate = homestate
   this.training = training
+  this.veto = function(){
+    return "No, I must disagree"
+  }
+  this.approve = function(){
+    return "No, I must disagree"
+  }
 }
